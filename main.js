07327c2e55273a8614ce921075ca1c706cea0560
skyllpro <huuphuoc2304@gmail.com>
2024-05-06 14:05:17 +0700

    Update main.js videoAds end time

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ if (document.location.host === "www.youtube.com") {
   function loop() {
     setTimeout(() => {
       let banners = Array.from(document.querySelectorAll(".ytp-ad-text-overlay .ytp-ad-overlay-close-container, .ytp-ad-image-overlay .ytp-ad-overlay-close-container"))
-      let videoAdBanner = document.querySelector(".ytp-ad-player-overlay-instream-info")
+      let videoAdBanner = document.querySelector(".ytp-ad-player-overlay-layout")
 
       if (banners.length > 0) {
         Array.from(banners).forEach((e) => {e.click()})
@@ -11,16 +11,19 @@ if (document.location.host === "www.youtube.com") {
 
       if (videoAdBanner !== null) {
         if (!currentlyPlayingAd) {
-          document.querySelector("video").muted = true
+          var videoAds = document.querySelector("video")
+          videoAds.muted = true
+          videoAds.currentTime = videoAds.duration
           currentlyPlayingAd = true
         }
-        let skipAd = document.querySelector("button.ytp-ad-skip-button-modern")
+        let skipAd = document.querySelector("button.ytp-skip-ad-button")
         if (skipAd) {
           skipAd.click()
         }
       } else {
         if (currentlyPlayingAd) {
-          document.querySelector("video").muted = false
+          var videoAds = document.querySelector("video")
+          videoAds.muted = false
           currentlyPlayingAd = false
         }
       }
